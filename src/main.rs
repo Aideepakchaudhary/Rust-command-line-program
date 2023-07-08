2a26e24360059d04dbd7de2ab845144615276d6c
Aideepakchaudhary <aideepakchaudhary@gmail.com>
2023-07-08 22:20:09 +0530

    accept-command-line-argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env; //return an iterator of the command line arguments.
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); // collect() return all the elements from the command line argument.
+    dbg!(args);
 }
