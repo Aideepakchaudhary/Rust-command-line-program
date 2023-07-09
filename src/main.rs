3db654e0fc4974bfa2f89b64c25d45667e1ba1d2
Aideepakchaudhary <aideepakchaudhary@gmail.com>
2023-07-09 13:40:16 +0530

    read file data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use std::env; //return an iterator of the command line arguments.
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // collect() return all the elements from the command line argument.
 
+    let (query, file_path) = parse_config(&args);
+    let content = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("with text:\n{content}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    (query,file_path)
 }
